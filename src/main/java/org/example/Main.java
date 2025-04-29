99b5402e6e0ed97be94e03b59527a3abaf1eaa2f
Matthew Pereira <6245576@edu.vaniercollege.qc.ca>
2025-04-29 15:21:06 -0400

    add insertion sort in Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {3, 1, 5, 2, 1, 7,5};
+        int[] nums = {3, 1, 5, 2, 1, 7, 5};
         bubbleSort(nums);
         System.out.println(Arrays.toString(nums));
         selectionSort(nums);
+        System.out.println(Arrays.toString(nums));
+        insertionSort(nums);
         System.out.println(Arrays.toString(nums));
     }
 
@@ -42,5 +44,19 @@ public class Main {
                 }
             }
         }
+    }
+
+    public static void insertionSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+
+            nums[j+1] = key;
+        }
+
     }
 }
