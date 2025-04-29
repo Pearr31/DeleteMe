1f8888d5ea6879cd8eefd373eea6d419ec2a4295
Matthew Pereira <6245576@edu.vaniercollege.qc.ca>
2025-04-29 15:05:27 -0400

    added selection sort method in main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,19 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {3, 1, 5, 2, 1, 4};
+        int[] nums = {3, 1, 5, 2, 1, 7,5};
         bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
+        selectionSort(nums);
         System.out.println(Arrays.toString(nums));
     }
 
+
+    /**
+     * sorts array of integers acsendingly
+     *
+     * @param nums input array
+     */
     public static void bubbleSort(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length - 1 - i; j++) {
@@ -21,4 +29,18 @@ public class Main {
         }
     }
 
+    public static void selectionSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[minIdx]) {
+                    minIdx = j;
+                    //swap nums[i] and nums[minIdx]
+                    int temp = nums[i];
+                    nums[i] = nums[minIdx];
+                    nums[minIdx] = temp;
+                }
+            }
+        }
+    }
 }
